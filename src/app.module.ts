bc55ee48a44d60ae523b9ae3f8cca6dd52fd5499
usmanaftab1234 <aftab.usman@funavry.com>
2024-04-26 00:53:36 +0500

    feat(app): DB connect with postgres and make docker-compose for postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,9 +9,21 @@ import { PaymentsModule } from './payments/payments.module';
 import { ReviewsService } from './reviews/reviews.service';
 import { ReviewsController } from './reviews/reviews.controller';
 import { ReviewsModule } from './reviews/reviews.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5433,
+      username: 'your_username',
+      password: 'your_password',
+      database: 'your_database_name',
+      // entities: [User], // Add other entities here
+      synchronize: true,
+    }), // Will automatically load ormconfig.json
+    // TypeOrmModule.forFeature([User]), // Add other entities here
     UsersModule,
     WalletsModule,
     TransactionsModule,
